b38638524212dc7c14c7bf0f26e4fcb9f586573a
Eristadevi <161942381+Eristadevi@users.noreply.github.com>
2025-09-16 13:58:00 +0700

    crud kelas dan siswa

---
+++ siakad-backend/knexfile.js
@@ -0,0 +1,36 @@
+import { config } from "dotenv";
+config();
+
+const knexConfig = {
+  development: {
+    client: process.env.DB_CLIENT || "mysql2",
+    connection: {
+      host: process.env.DB_HOST || "127.0.0.1",
+      port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
+      user: process.env.DB_USER || "root",
+      password: process.env.DB_PASS || "",
+      database: process.env.DB_NAME || "be-sistem-siakad",
+    },
+    migrations: {
+      directory: "./src/migrations",
+      extension: "js",
+      loadExtensions: [".js"],
+    },
+  },
+  production: {
+    client: process.env.DB_CLIENT || "mysql2",
+    connection: {
+      host: process.env.DB_HOST || "127.0.0.1",
+      port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
+      user: process.env.DB_USER || "root",
+      password: process.env.DB_PASS || "",
+      database: process.env.DB_NAME || "siakad_pt",
+    },
+    migrations: {
+      directory: "./src/migrations",
+      extension: "js",
+    },
+  },
+};
+
+export default knexConfig;
---
+++ siakad-backend/src/app.js
@@ -0,0 +1,49 @@
+import cors from "cors";
+import express from "express";
+import logger from "morgan";
+
+import { setResponseHeader } from "./middleware/set-headers.js";
+import siswaRoutes from "./routes/siswaRoutes.js";
+import kelasRoutes from "./routes/kelasRoutes.js";
+
+const app = express();
+
+const allowedOrigins = ["http://localhost:3000"];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Timestamp",
+      "X-Signature",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    optionSuccessStatus: 200,
+  }),
+);
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Root endpoint
+app.get("/", [setResponseHeader], (req, res) => {
+  return res
+    .status(200)
+    .json(`Welcome to the server! ${new Date().toLocaleString()}`);
+});
+
+// Register semua routes
+app.use("/api/siswa", siswaRoutes);
+app.use("/api/kelas", kelasRoutes);
+
+export default app;
