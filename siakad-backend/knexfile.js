ed7ab1c0b3338296b11683a57a5d7a9b765c908d
AuliaDiva1 <auliadva123@gmail.com>
2025-09-17 11:04:42 +0700

    Menambahkan perubahan pada knexfile.js

--- siakad-backend/knexfile.js
+++ siakad-backend/knexfile.js
@@ -1,54 +1,3 @@
-<<<<<<< HEAD
-import dotenv from 'dotenv';
-
-dotenv.config();
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
-const config = {
-  development: {
-    client: process.env.DB_CLIENT,
-    connection: {
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      directory: './src/migrations',
-      extension: 'js',
-      loadExtensions: ['.js'],
-    },
-  },
-
-  production: {
-    client: process.env.DB_CLIENT,
-    connection: {
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      directory: './src/migrations',
-      extension: 'js',
-    }
-  }
-
-};
-
-export default config;
-=======
 import { config } from "dotenv";
 config();
 
@@ -85,4 +34,4 @@ const knexConfig = {
 };
 
 export default knexConfig;
->>>>>>> b38638524212dc7c14c7bf0f26e4fcb9f586573a
+
